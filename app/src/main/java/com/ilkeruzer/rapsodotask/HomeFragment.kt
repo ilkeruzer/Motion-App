8d393a48de8085f40c01569967762a60f0afc864
ilkeruzer <ilkeruzer35@gmail.com>
2022-04-26 18:37:00 +0300

    Home fragment added and navigate motion fragment.

---
+++ app/src/main/java/com/ilkeruzer/rapsodotask/HomeFragment.kt
@@ -0,0 +1,39 @@
+package com.ilkeruzer.rapsodotask
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.ilkeruzer.rapsodotask.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.floatingActionButton.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToMotionFragment()
+            findNavController().navigate(action)
+        }
+    }
+
+}
